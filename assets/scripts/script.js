babdbefac15826797e6097aa6862fc03e0eb983a
UZBase <sharofiddinov.07@mail.ru>
2021-12-28 14:32:03 +0300

    Update script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,20 +5,18 @@ window.addEventListener('load', function (e) {
     const resurs_4 = document.querySelector('.resurs_4')
     const resurs_5 = document.querySelector('.resurs_5')
     const resurs_6 = document.querySelector('.resurs_6')
-    const resurs_other = document.querySelector('.resurs_7')
+    const resurs_7 = document.querySelector('.resurs_7')
 
-    resurs_1.click(function () {
-        if (resurs_1.classList.contains("button-active")) {
-            resurs_1.classList.remove("button-active");
-        } else {
-            resurs_1.classList.add("button-active");
-            resurs_2.classList.remove("button-active");
-            resurs_3.classList.remove("button-active");
-            resurs_4.classList.remove("button-active");
-            resurs_5.classList.remove("button-active");
-            resurs_6.classList.remove("button-active");
-            resurs_other.classList.remove("button-active");
-        }
+    const arr = new Array(resurs_1,resurs_2,resurs_3,resurs_4,resurs_5,resurs_6,resurs_7)
+    console.log(arr);
+    arr.forEach(btn=>{
+        btn.addEventListener('click',function () {
+            arr.forEach((elem)=>{
+                elem.classList.remove('button-active')
+            })
+            this.classList.add('button-active')
+    })
+       
     });
 });
 
