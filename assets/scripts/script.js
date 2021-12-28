ffad597b06e2695fb2d0b7c834d0f8672eee81a5
UZBase <sharofiddinov.07@mail.ru>
2021-12-28 14:21:38 +0300

    push

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,24 @@
 window.addEventListener('load', function (e) {
-    const arr = [...document.querySelectorAll('.budget-button'), ...document.querySelectorAll('.resurs-button')]
-    for (var i = 0; i < arr.length; i++) {
-        arr[i].addEventListener('click', function (event) {
-            console.log('work');
-        });
-    }
-    const necessary_input = document.querySelector('necessary-input')
-    const resurs_input = document.querySelector('resurs-input')
+    const resurs_1 = document.querySelector('.resurs_1')
+    const resurs_2 = document.querySelector('.resurs_2')
+    const resurs_3 = document.querySelector('.resurs_3')
+    const resurs_4 = document.querySelector('.resurs_4')
+    const resurs_5 = document.querySelector('.resurs_5')
+    const resurs_6 = document.querySelector('.resurs_6')
+    const resurs_other = document.querySelector('.resurs_7')
+
+    resurs_1.click(function () {
+        if (resurs_1.classList.contains("button-active")) {
+            resurs_1.classList.remove("button-active");
+        } else {
+            resurs_1.classList.add("button-active");
+            resurs_2.classList.remove("button-active");
+            resurs_3.classList.remove("button-active");
+            resurs_4.classList.remove("button-active");
+            resurs_5.classList.remove("button-active");
+            resurs_6.classList.remove("button-active");
+            resurs_other.classList.remove("button-active");
+        }
+    });
 });
 
