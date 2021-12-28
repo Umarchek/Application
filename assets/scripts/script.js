7b94a49fec9b0b5735e989e0ffbd63711c0c6309
UZBase <sharofiddinov.07@mail.ru>
2021-12-28 17:07:18 +0300

    fixed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,17 +6,28 @@ window.addEventListener('load', function (e) {
     const resurs_5 = document.querySelector('.resurs_5')
     const resurs_6 = document.querySelector('.resurs_6')
     const resurs_7 = document.querySelector('.resurs_7')
+    const drugoe = document.querySelector('#drugoe')
+    const resurs_buttons = document.querySelector('.resurs-buttons')
 
-    const arr = new Array(resurs_1,resurs_2,resurs_3,resurs_4,resurs_5,resurs_6,resurs_7)
-    console.log(arr);
-    arr.forEach(btn=>{
-        btn.addEventListener('click',function () {
-            arr.forEach((elem)=>{
+
+    const resurs_input = document.querySelector('.resurs-input')
+    const arr = new Array(resurs_1, resurs_2, resurs_3, resurs_4, resurs_5, resurs_6, resurs_7)
+    arr.forEach(btn => {
+        btn.addEventListener('click', function () {
+            arr.forEach((elem) => {
                 elem.classList.remove('button-active')
+                resurs_buttons.style.marginBottom = '50px'
             })
             this.classList.add('button-active')
-    })
-       
+            if (drugoe.classList.contains("button-active")) {
+                resurs_input.style.display = 'block'
+                resurs_buttons.style.marginBottom = '0px'
+            } else {
+                resurs_input.style.display = 'none'
+                resurs_buttons.style.marginBottom = '150px'
+            }
+        })
     });
+
 });
 
