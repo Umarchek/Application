b5bbc200a266fd4880dcc09638185c8d06ec221c
UZBase <sharofiddinov.07@mail.ru>
2021-12-28 14:01:37 +0300

    added class .button-active

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,11 @@
-window.addEventListener('load', function(e) {
-    // body
+window.addEventListener('load', function (e) {
+    const arr = [...document.querySelectorAll('.budget-button'), ...document.querySelectorAll('.resurs-button')]
+    for (var i = 0; i < arr.length; i++) {
+        arr[i].addEventListener('click', function (event) {
+            console.log('work');
+        });
+    }
+    const necessary_input = document.querySelector('necessary-input')
+    const resurs_input = document.querySelector('resurs-input')
 });
+
